f2f1cd50ef3e5a14765a76eb17d19165b02b2b73
FeiLiuEM <feiliuem@outlook.com>
2022-09-05 19:46:08 +0800

    Update web.py

--- web.py
+++ web.py
@@ -33,6 +33,7 @@ class MyApp(App):
         self.bt = gui.Button('提交')
         self.bt2 = gui.Button('查询结果')
         self.download=gui.FileDownloader(text='',filename='/media/math/DATA/RESULT/CIRP.tar.gz')
+        self.lbl4 = gui.Label('引用文献：Fei Liu et.al, A chronotherapeutics-applicable multi-target therapeutics based on AI: the example of therapeutic hypothermia, Briefings in Bioinformatics, DOI:10.1093/bib/bbac365.')
 
         # setting the listener for the onclick event of the buttons
         self.bt.onclick.do(self.on_button_pressed)
@@ -49,7 +50,8 @@ class MyApp(App):
         container.append(self.input2)
         container.append(self.bt2)
         container.append(self.lbl3)
-        container.append(self.download)        
+        container.append(self.download)   
+        container.append(self.lbl4)
         
         # returning the root widget
         return container
