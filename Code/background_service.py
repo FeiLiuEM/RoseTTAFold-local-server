37158ac7910d960dfd2f67d071bb8183bf2714ad
FeiLiuEM <feiliuem@outlook.com>
2022-12-08 13:57:46 +0800

    Update background_service.py

--- Code/background_service.py
+++ Code/background_service.py
@@ -21,17 +21,17 @@ while True:
 while True:
     time.sleep(30) #时间间隔，单位是秒，每隔30秒运行一次
 
-    apply=pd.read_csv("/home/math/DATA/RoseTTAFold/apply.csv")    # your apply list here 申请文件，所有申请而没有运行的数据都保存在这里
-    result=pd.read_csv("/home/math/DATA/RoseTTAFold/result.csv")  # your result list here 结果文件
+    apply=pd.read_csv("RunningData/apply.csv")    # your apply list here 申请文件，所有申请而没有运行的数据都保存在这里
+    result=pd.read_csv("RunningData/result.csv")  # your result list here 结果文件
 
     #判定是否删除运行指示器并且对文件进行压缩处理
-    if os.path.exists("/home/math/DATA/RoseTTAFold/running/model/model_1.pdb") == True:     # check if it is running
+    if os.path.exists("RunningData/running/model/model_1.pdb") == True:     # check if it is running
         
         #apply=pd.read_csv("/Users/liufei/Desktop/test.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
 
         tar_filename=str(apply.values.tolist()[0][1])+".tar.gz"
-        resultpath=r"/home/math/DATA/RoseTTAFold/"+tar_filename            # your apply file here
-        targetpath=r"/media/math/DATA/RESULT/RoseTTAFold/"+tar_filename    # your result folder here
+        resultpath=r"RunningData/"+tar_filename            # your apply file here
+        targetpath=r"Result/"+tar_filename    # your result folder here
         newline=apply.values.tolist()[0]
         newline.append(targetpath)
         
@@ -40,30 +40,30 @@ while True:
         result_list.append(newline)
         new_result=pd.DataFrame(result_list,columns=['Date', 'Project_code', 'Amino_acid_sequence', 'Target_path'])
         
-        new_result.to_csv("/home/math/DATA/RoseTTAFold/result.csv",index=False,sep=',')
+        new_result.to_csv("RunningData/result.csv",index=False,sep=',')
 
         #apply文件的修改及写入
         apply_new=apply.values.tolist()
         apply_new=pd.DataFrame(apply_new[1:],columns=['Date', 'Project_code', 'Amino_acid_sequence'])
-        apply_new.to_csv("/home/math/DATA/RoseTTAFold/apply.csv",index=False,sep=',')
+        apply_new.to_csv("RunningData/apply.csv",index=False,sep=',')
 
 
         #创建结果压缩包，移动到新文件夹中，并且初始化运行文件夹。
-        instruction=r"cd /home/math/DATA/RoseTTAFold ;tar -zcvf "+tar_filename  +r" /home/math/DATA/RoseTTAFold/running ;mv " + resultpath +r" /media/math/DATA/RESULT/RoseTTAFold"
-        os.system(r'cp /home/math/DATA/RoseTTAFold/running/model/model_1.crderr.pdb /media/math/DATA/RESULT/RoseTTAFold/pdb/'+str(apply.values.tolist()[0][1])+'.pdb')
+        instruction=r"cd RunningData ;tar -zcvf "+tar_filename  +r" /home/math/DATA/RoseTTAFold/running ;mv " + resultpath +r" /media/math/DATA/RESULT/RoseTTAFold"
+        os.system(r'cp RunningData/running/model/model_1.crderr.pdb Result/pdb/'+str(apply.values.tolist()[0][1])+'.pdb')
         os.system(instruction)
-        os.system(r"rm -rf /home/math/DATA/RoseTTAFold/running")
-        os.system(r"mkdir /home/math/DATA/RoseTTAFold/running")
+        os.system(r"rm -rf RunningData/running")
+        os.system(r"mkdir RunningData/running")
 
-        apply=pd.read_csv("/home/math/DATA/RoseTTAFold/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
-        result=pd.read_csv("/home/math/DATA/RoseTTAFold/result.csv") #结果文件
+        apply=pd.read_csv("RunningData/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
+        result=pd.read_csv("RunningData/result.csv") #结果文件
 
 
 
 #通过申请文件及运行指示器（fa文件）判定是否运行下一个蛋白
     if apply.values.tolist() != []:
         #判定运行文件是否存在
-        if os.path.exists("/home/math/DATA/RoseTTAFold/running/test.fa") == False:
+        if os.path.exists("RunningData/running/test.fa") == False:
             RNA = apply.values.tolist()[0][2]
             project_code = apply.values.tolist()[0][0]
             
@@ -78,39 +78,39 @@ while True:
                 result_list.append(same)
                 new_result=pd.DataFrame(result_list,columns=['Date', 'Project_code', 'Amino_acid_sequence', 'Target_path'])
 
-                new_result.to_csv("/home/math/DATA/RoseTTAFold/result.csv",index=False,sep=',')
+                new_result.to_csv("RunningData/result.csv",index=False,sep=',')
 
                 #apply文件的修改及写入
                 apply_new=apply.values.tolist()
                 apply_new=pd.DataFrame(apply_new[1:],columns=['Date', 'Project_code', 'Amino_acid_sequence'])
-                apply_new.to_csv("/home/math/DATA/RoseTTAFold/apply.csv",index=False,sep=',')
+                apply_new.to_csv("RunningData/apply.csv",index=False,sep=',')
                 
-                apply=pd.read_csv("/home/math/DATA/RoseTTAFold/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
-                result=pd.read_csv("/home/math/DATA/RoseTTAFold/result.csv") #结果文件
+                apply=pd.read_csv("RunningData/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
+                result=pd.read_csv("RunningData/result.csv") #结果文件
 
             
             else:
                 fa=[">test |"]
                 fa.append(RNA)
 
-                f = open("/home/math/DATA/RoseTTAFold/running/test.fa","w",encoding='utf-8')
+                f = open("RunningData/running/test.fa","w",encoding='utf-8')
 
                 for line in fa:
                     f.write(line+'\n')
                 f.close()
 
-                os.system("cd /home/math/DATA/RoseTTAFold;./apply.sh")   #运行计算程序
+                os.system("cd Code;./apply.sh")   #运行计算程序
                 notification = "正在运行 "+apply.values.tolist()[0][1]
                 print(notification)
 
-                apply=pd.read_csv("/home/math/DATA/RoseTTAFold/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
-                result=pd.read_csv("/home/math/DATA/RoseTTAFold/result.csv") #结果文件
+                apply=pd.read_csv("RunningData/apply.csv")  #申请文件，所有申请而没有运行的数据都保存在这里
+                result=pd.read_csv("RunningData/result.csv") #结果文件
 
 
 
 
 
-
+'''
 apply=pd.read_csv("/Users/liufei/Downloads/apply.csv")
 result=pd.read_csv("/Users/liufei/Downloads/result.csv")
 
@@ -125,3 +125,4 @@ same[1]=apply.values.tolist()[0][1]
 result_list=result.values.tolist()
 result_list.append(same)
 new_result=pd.DataFrame(result_list,columns=['Date', 'Project_code', 'Amino_acid_sequence', 'Target_path'])
+'''
