2599f576e5feadb8a0744f77b92a54fa0289e045
FeiLiuEM <feiliuem@outlook.com>
2022-12-09 10:22:45 +0800

    Update apply.sh

--- Code/apply.sh
+++ Code/apply.sh
@@ -1,1 +1,1 @@
-conda activate RoseTTAFold; cd ThePathOf/RoseTTAFold; ./run_pyrosetta_ver.sh RunningData/running/test.fa RunningData/running
+conda init bash; conda activate RoseTTAFold; cd ThePathOf/RoseTTAFold; ./run_pyrosetta_ver.sh RunningData/running/test.fa RunningData/running
